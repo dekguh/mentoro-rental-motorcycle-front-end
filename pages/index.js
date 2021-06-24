c1f02cd557f5b71f521b1236473f8c99e2d57f19
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-24 20:08:32 +0800

    create add discount section

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head';
 import Image from 'next/image';
 import HomeHead from '../components/organisms/HomeHead';
 import TypeMotorList from '../components/organisms/TypeMotorList';
+import DiscountSection from '../components/organisms/DiscountSection';
+import dataMotor from '../components/utils/data/dataMotor';
 
-export default function Home() {
+export default function Home({ dataDiscount }) {
   return (
     <div className='home__wrapper'>
       <div className='home__search-wrap'>
@@ -12,8 +14,20 @@ export default function Home() {
       </div>
 
       <div className='home__content-wrap'>
-        home content
+        <DiscountSection dataDiscount={dataDiscount} />
       </div>
     </div>
   )
+}
+
+
+export async function getServerSideProps() {
+  const filtered = dataMotor.filter(data => data.isDiscount == true);
+  const sorted = filtered.sort((a, b) => b.id - a.id);
+  const limit = sorted.slice(0, 2)
+  return {
+    props: {
+      dataDiscount: limit
+    }
+  }
 }
