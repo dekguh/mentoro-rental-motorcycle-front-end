ba7e81208d7a96be92f23e848adab1ab2bd168ea
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-24 11:25:51 +0800

    update type motor

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import HomeHead from '../components/organisms/HomeHead';
+import TypeMotorList from '../components/organisms/TypeMotorList';
 
 export default function Home() {
   return (
     <div className='home__wrapper'>
       <div className='home__search-wrap'>
         <HomeHead />
+        <TypeMotorList classes='margin-top-16' />
       </div>
 
       <div className='home__content-wrap'>
