268514eb504ba255f222e168140bcb8bdd676ca0
Dekguh <kadekteguhmahesa@gmail.com>
2021-06-24 20:07:17 +0800

    create discount section

---
+++ components/organisms/DiscountSection/index.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import TitleSection from '../../molecules/TitleSection'
+import CardMotorHorizontal from '../../molecules/card/CardMotorHorizontal'
+
+const DiscountSection = ({classes, dataDiscount, ...rest}) => {
+    return (
+        <div className={classes} {...rest}>
+            <TitleSection component='h4' text='Discount' classes='margin-bottom-16' />
+            <ul className='list__discount'>
+            {dataDiscount && dataDiscount.map((data, i) => (
+                <li key={i} className='margin-bottom-8'>
+                    <CardMotorHorizontal
+                        img={data.thumbnailURL}
+                        url='#'
+                        title={data.name}
+                        pricePerHour={data.rent_price.priceList[0].pricePerHour}
+                    />
+                </li>
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+export default DiscountSection
