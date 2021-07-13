bb9364afe70110b18547634712b257d0d971fc64
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-13 12:31:20 +0800

    create success & failed booking page

---
+++ pages/booking/failed.js
@@ -0,0 +1,15 @@
+import NotifPageSection from '../../components/organisms/NotifPageSection'
+
+const FailedPaymentPage = () => {
+    return (
+        <div>
+            <NotifPageSection
+                image='/images/flat-cancel.png'
+                title='Oops! your order cancel/failed'
+                description='payment has been successfully cancelled, if you have any questions please contact us immediately'
+            />
+        </div>
+    )
+}
+
+export default FailedPaymentPage
--- pages/booking/success.js
+++ pages/booking/success.js
@@ -1,13 +1,12 @@
-import React from 'react'
 import NotifPageSection from '../../components/organisms/NotifPageSection'
 
 const SuccessPaymentPage = () => {
     return (
         <div>
             <NotifPageSection
-                image='/images/flat-cancel.png'
-                title='Oops! your order cancel/failed'
-                description='payment has been successfully cancelled, if you have any questions please contact us immediately'
+                image='/images/flat-success.png'
+                title='Thanks you! your order success'
+                description='payment has been successfully, if you have any questions please contact us immediately'
             />
         </div>
     )
