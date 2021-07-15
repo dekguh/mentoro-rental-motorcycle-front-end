b301e6571f8e5304387bc86ecc128268697e1e60
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-15 17:27:42 +0800

    weback fs: false

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ const path = require('path');
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
-    }
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.node = {
+          fs: 'empty'
+        }
+        return config
+    },
 }
