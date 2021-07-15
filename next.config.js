49ec41d0a82c6afe053167d912aaf461ccafb4bb
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-15 17:32:59 +0800

    fix fs module error

--- next.config.js
+++ next.config.js
@@ -3,11 +3,5 @@ const path = require('path');
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
-    },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.node = {
-          fs: 'empty'
-        }
-        return config
-    },
+    }
 }
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const webpackConfig = {
+    target: 'node'
+  };
+  
+  module.exports = webpackConfig;
