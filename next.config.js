886271d1c7ae94545d03704180c943c326ce73f8
Dekguh <kadekteguhmahesa@gmail.com>
2021-07-15 18:13:44 +0800

    oke finally fix >:)

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const path = require('path');
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')]
-    }
+    },
+    Webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+        return config
+      }
 }
